459d96f2c4244f4b5191cceaf6f57362ec0eea75
TruongPQ <truongphamit@gmail.com>
2017-04-19 17:06:24 +0700

    add Screenshot

---
+++ app/src/main/java/com/pqs/mediaplayer/fragments/SettingsFragment.java
@@ -0,0 +1,73 @@
+package com.pqs.mediaplayer.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pqs.mediaplayer.R;
+
+/**
+ * Created by truongpq on 19/04/2017.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    public static final String KEY_SOUND_EFFECTS = "pre_sound_effects";
+    public static final String KEY_SLEEP_TIMER = "pre_sleep_timer";
+    public static final String KEY_PAUSE_ON_DISCONNECT = "pre_pause_on_disconnect";
+    public static final String KEY_RESUME_ON_CONNECT = "pre_resume_on_connect";
+
+    public static SettingsFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        SettingsFragment fragment = new SettingsFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preference, rootKey);
+
+
+        Preference pre_sound_effects = findPreference(KEY_SOUND_EFFECTS);
+        pre_sound_effects.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+
+                return false;
+            }
+        });
+
+        Preference pre_sleep_timer = findPreference(KEY_SLEEP_TIMER);
+        pre_sleep_timer.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+
+                return false;
+            }
+        });
+
+        Preference pre_pause_on_disconnect = findPreference(KEY_PAUSE_ON_DISCONNECT);
+        pre_pause_on_disconnect.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+
+                return false;
+            }
+        });
+
+        Preference pre_resume_on_connect = findPreference(KEY_RESUME_ON_CONNECT);
+        pre_resume_on_connect.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
